cfa61896df5fe25588273a7ca525ae0ae9e15ff6
Mampuru <mampuruntwampe@gmail.com>
2022-03-16 00:38:57 +0200

    initial template for thanks website

---
+++ src/components/Navbars/AuthNavbar.vue
@@ -0,0 +1,111 @@
+<template>
+  <nav
+    class="top-0 fixed z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-white shadow"
+  >
+    <div
+      class="container px-4 mx-auto flex flex-wrap items-center justify-between"
+    >
+      <div
+        class="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start"
+      >
+        <router-link to="/">
+          <a
+            class="text-blueGray-700 text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
+            href="#pablo"
+          >
+            <img 
+            alt="ThanX Logo" 
+            :src="thanx_logo"
+            class="mx-auto max-w-120-px "/>
+          </a>
+        </router-link>
+        <button
+          class="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+          type="button"
+          v-on:click="setNavbarOpen"
+        >
+          <i class="fas fa-bars"></i>
+        </button>
+      </div>
+      <div
+        class="lg:flex flex-grow items-center"
+        :class="[navbarOpen ? 'block' : 'hidden']"
+        id="example-navbar-warning"
+      >
+        <!-- <ul class="flex flex-col lg:flex-row list-none mr-auto">
+          <li class="flex items-center">
+            <a
+              class="hover:text-blueGray-500 text-blueGray-700 px-3 py-2 flex items-center text-xs uppercase font-bold"
+              href="https://www.creative-tim.com/learning-lab/tailwind/vue/overview/notus?ref=vn-index-navbar"
+            >
+              Docs
+            </a>
+          </li>
+        </ul> -->
+        <ul class="flex flex-col lg:flex-row list-none lg:ml-auto">
+          <li class="flex items-center">
+             <router-link to="/">
+              <a
+                class="text-blueGray-700 text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap "
+                href="#pablo"
+              >
+                About Us
+              </a>
+              </router-link>
+          </li>
+          <!-- <li class="flex items-center">
+             <router-link to="/">
+              <a
+                class="text-blueGray-700 text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap "
+                href="#pablo"
+              >
+                Our Values
+              </a>
+              </router-link>
+          </li> -->
+          <li class="flex items-center">
+             <router-link to="/">
+              <a
+                class="text-blueGray-700 text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap "
+                href="#pablo"
+              >
+                Services
+              </a>
+              </router-link>
+          </li>
+          <li class="flex items-center">
+             <router-link to="/auth/login">
+              <a>
+                <i class="text-blueGray-900 fas fa-user-circle text-lg leading-lg" />
+                <span class="inline-block ml-2 ">Sign in</span>
+              </a>
+             </router-link>
+              
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+// import IndexDropdown from "@/components/Dropdowns/IndexDropdown.vue";
+import thanx_logo from "@/assets/img/thanx_logo.png";
+
+export default {
+  data() {
+    return {
+      navbarOpen: false,
+      thanx_logo,
+    };
+  },
+  methods: {
+    setNavbarOpen: function () {
+      this.navbarOpen = !this.navbarOpen;
+    },
+  },
+  components: {
+    // IndexDropdown,
+  },
+};
+</script>
