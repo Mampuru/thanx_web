9674b2a06b18d1e7d97fb873cb73d5de52b88964
Mampuru <mampuruntwampe@gmail.com>
2022-03-30 03:42:11 +0200

    API connection CORS config

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -57,7 +57,7 @@
                 >
                   <div class="py-6 px-3 mt-32 sm:mt-0">
                     <button
-                      class="bg-emerald-500 active:bg-emerald-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
+                      class="bg-blueGray-700 active:bg-blueGray-800 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
                       type="button"
                     >
                       Edit Profile
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 // vue.config.js
 module.exports = {
   runtimeCompiler: true,
+  devServer: {
+    proxy: 'http://api.mythanx.xyz/',
+    }
 };
