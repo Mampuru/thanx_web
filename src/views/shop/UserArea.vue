cc56f8a0187119f97aa11d4de49a344d0ba9a03f
Mampuru <mampuruntwampe@gmail.com>
2022-03-31 02:14:06 +0200

    Centering of content

--- src/views/shop/UserArea.vue
+++ src/views/shop/UserArea.vue
@@ -1,9 +1,11 @@
+
 <template>
   <div>
     <navbar />
-    <main class="" style="margin-top: 5rem; margin-bottom:5rem; padding:">
-      <div class="flex flex-wrap">
-        <div class="w-full md:w-5/12 px-4 flex-1" style="padding: 5rem;">
+    <main style="margin-top: 5rem; margin-bottom:5rem;">
+      <div class="container mx-auto px-4">
+        <div class="items-center flex flex-wrap">
+        <div class="w-full md:w-8/12 px-4 flex-1" style="padding: 5rem;">
           <div class="bg-lightBlue-700 shadow-lg rounded-lg text-center p-8">
             <i class="fas fa-wallet text-white text-3xl"></i>
             <p class="text-3xl text-white mt-4 font-semibold">Subscriptions</p>
@@ -20,7 +22,7 @@
         <div class="w-full md:w-5/12 px-4 flex-1" style="padding: 5rem;">
           <div class="bg-lightBlue-700 shadow-lg rounded-lg text-center p-8">
             <i class="fas fa-wallet text-white text-3xl"></i>
-            <p class="text-3xl text-white mt-4 font-semibold">Rewards Bundle</p>
+            <p class="text-2xl text-white mt-4 font-semibold">Rewards Bundle</p>
             <p class="text-3xl text-white mt-4 font-semibold">Balance: 0.0</p>
           </div>
         </div>
@@ -46,6 +48,7 @@
             <p class="text-3xl text-white mt-4 font-semibold">Balance: 0.0</p>
           </div>
         </div>
+      </div>
       </div>
     </main>
     <footer-component />
