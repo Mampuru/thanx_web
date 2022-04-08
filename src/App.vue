775a43bbabad3d86a801a22b17c17afda88ae9fe
Ngoy Lufo <ngoylufo@gmail.com>
2022-04-08 14:25:21 +0200

    Removed Loader component and applied formatting

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
 <template>
-  <div>
-    <router-view />
-  </div>
+	<div>
+		<router-view />
+	</div>
 </template>
 <script>
-import {useStore} from "vuex";
-import { computed} from 'vue';
-import Loader from "./components/Loader/Loader.vue";
+import { useStore } from "vuex";
+import { computed } from "vue";
 
-//     import('./components/Loader/Loader.vue'),
-// );
 export default {
-  name: 'App',
-  setup() {
-    const store = useStore();
-    const showLoading = computed(() => store.state.showLoading)
+	name: "App",
+	setup() {
+		const store = useStore();
+		const showLoading = computed(() => store.state.showLoading);
 
-    return{
-      showLoading
-    }
-  },
-  components: {
-    Loader
-  },
-  created() {
-        this.$store.dispatch(`autoLogin`);
-    },
-}
+		return {
+			showLoading,
+		};
+	},
+	created() {
+		this.$store.dispatch(`autoLogin`);
+	},
+};
 </script>
